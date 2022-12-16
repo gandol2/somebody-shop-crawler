aa3530c2a915fb059aaa7074608dbe083c1af30f
gandol2 <sskim2333@gmail.com>
2022-12-16 12:35:37 +0900

    temp

--- dist/libs/db/channelHelper.js
+++ dist/libs/db/channelHelper.js
@@ -1,7 +1,18 @@
 import prisma from "../prismadb.js";
 import { getDayDate } from "../dateHelper.js";
+function rand(start, end) {
+    return Math.floor(Math.random() * (end - start + 1) + start);
+}
 export async function getLastUpdatedChannel() {
-    return await prisma.channel.findFirst({ orderBy: { updateDay: "asc" } });
+    const channels = await prisma.channel.findMany({
+        take: 10,
+        orderBy: { updateDay: "asc" },
+    });
+    // console.log(channels);
+    const idx = rand(0, channels.length);
+    const channel = channels[idx];
+    console.log(idx);
+    return channel;
 }
 export async function updateChannel(channelId, option) {
     return await prisma.channel.update({
