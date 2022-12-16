80a0dbc8d6f10e4fc72ec3c2a6e7040e811d0989
gandol2 <sskim2333@gmail.com>
2022-12-16 12:37:45 +0900

    temp

--- libs/db/channelHelper.ts
+++ libs/db/channelHelper.ts
@@ -19,7 +19,7 @@ export async function getLastUpdatedChannel() {
 
   const channel = channels[idx];
 
-  // console.log(idx);
+  console.log(`select channels = ${idx}/${channels.length}`);
 
   return channel;
 }
