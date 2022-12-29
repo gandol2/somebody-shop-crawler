23848b6fbb64288a8e87be178b2cb4f0dc64c76d
gandol2 <sskim2333@gmail.com>
2022-12-29 16:24:36 +0900

    temp

--- libs/channelIdHelper.ts
+++ libs/channelIdHelper.ts
@@ -2,15 +2,21 @@ import fs from "fs";
 import os from "os";
 import path from "path";
 import Dotenv from "dotenv";
-Dotenv.config();
+import appRootPath from "app-root-path";
+
+const ENV_PATH = path.join(appRootPath.toString(), ".env"); // ENV
+
+Dotenv.config({ path: ENV_PATH });
 
 // const ENV_PATH = path.join(process.env.PWD!, ".env"); // ENV
-const ENV_PATH = path.join(".env"); // ENV
+// const ENV_PATH = "../../../.env"; // ENV
 
 export function getChannelIDbyENV(): number | undefined {
   const STORE_NO_END = Number(process.env.STORE_NO_END);
   const SERVER_TOTAL = Number(process.env.SERVER_TOTAL);
   const STORE_NO_START = Number(process.env.STORE_NO_START);
+
+  console.log(STORE_NO_END);
 
   if (STORE_NO_END < STORE_NO_START) {
     return undefined;
@@ -22,7 +28,9 @@ export function getChannelIDbyENV(): number | undefined {
 
 export default function setEnvValue(key: string, value: any) {
   // read file from hdd & split if from a linebreak to a array
+  console.log("1");
   const ENV_VARS = fs.readFileSync(ENV_PATH, "utf8").split(os.EOL);
+  console.log("2");
 
   const search = ENV_VARS.find((line) => {
     return line.match(new RegExp(key));
